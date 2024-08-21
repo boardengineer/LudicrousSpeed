0a24dd471ebd4f451923361eaaa922bc2c5e6880
boardengineer <77598527+boardengineer@users.noreply.github.com>
2024-08-20 21:08:28 -0700

    save stuff to battleaimod

--- src/main/java/ludicrousspeed/simulator/patches/ScreenPatches.java
+++ src/main/java/ludicrousspeed/simulator/patches/ScreenPatches.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.core.OverlayMenu;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.monsters.MonsterGroup;
 import com.megacrit.cardcrawl.screens.CombatRewardScreen;
-import com.megacrit.cardcrawl.screens.DeathScreen;
 import ludicrousspeed.LudicrousSpeedMod;
 
 public class ScreenPatches {
@@ -33,35 +30,6 @@ public class ScreenPatches {
     public static class TooMdddanyLinesPatch {
         public static SpireReturn Prefix(OverlayMenu _instance) {
             if (LudicrousSpeedMod.plaidMode) {
-                return SpireReturn.Return(null);
-            }
-            return SpireReturn.Continue();
-        }
-    }
-
-    @SpirePatch(
-            clz = DeathScreen.class,
-            paramtypez = {MonsterGroup.class},
-            method = SpirePatch.CONSTRUCTOR
-    )
-    public static class DisableDeathScreenpatch {
-        public static SpireReturn Prefix(DeathScreen _instance, MonsterGroup monsterGroup) {
-            if (LudicrousSpeedMod.plaidMode) {
-                return SpireReturn.Return(null);
-            }
-            return SpireReturn.Continue();
-        }
-    }
-
-    @SpirePatch(
-            clz = DeathScreen.class,
-            paramtypez = {},
-            method = "reopen"
-    )
-    public static class DisableDeathReopenScreenpatch {
-        public static SpireReturn Prefix(DeathScreen _instance) {
-            if (LudicrousSpeedMod.plaidMode) {
-                AbstractDungeon.previousScreen = AbstractDungeon.CurrentScreen.DEATH;
                 return SpireReturn.Return(null);
             }
             return SpireReturn.Continue();
